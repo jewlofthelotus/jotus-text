8dea6d3d149e49d68e6b10b0a42ba0b3cd733f2c
Julie Bellinson <jewlofthelotus@gmail.com>
2013-09-19 23:23:24 -0400

    shortening excerpt on non-home page lists, adding comment count to posts

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
         <div class="content">
             <section class="post-list">
+    <?php $count = 0; ?>
     <?php while ( have_posts() ) : the_post() ?>
+    <?php $count++; ?>
                 <article class="post">
                     <div class="post-date">
                         <span class="day"><?php the_time('d'); ?></span>
@@ -12,7 +14,11 @@
                     <h2 class="post-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
                     <div class="post-content">
-        <?php the_content(); ?>
+<?php if ( $count < 2 && !is_home() ) : ?>
+        <?php the_content() ?>
+<?php else : ?>
+        <?php the_excerpt(); ?>
+<?php endif; ?>
                     </div>
 
                     <div class="post-meta">
@@ -20,13 +26,13 @@
                             get_the_category_list(', '),
                             get_the_tag_list(' '.__('and tagged').' ', ', ', '')) ?>
 
-        <?php if (('open' == $post-> comment_status)) : // Comments open ?>
-                        <?php printf(__('<a class="comment-link" href="#respond" title="Leave a comment">Leave a comment</a>.', 'jotus')) ?>
-        <?php elseif (!('open' == $post-> comment_status)) : // Comments are closed ?>
+                    <?php if ( comments_open() ) : ?>
+                        <strong><?php comments_popup_link( '<span class="leave-comment">' . __( 'Leave a comment', 'jotusText' ) . '</span>', __( '1 Comment', 'jotusText' ), __( '% Comments', 'jotusText' ) ); ?></strong>
+                    <?php else : ?>
                         <?php _e('Comments are currently closed.') ?>
-        <?php endif; ?>
+                    <?php endif; // comments_open() ?>
 
-        <?php edit_post_link(__('Edit', 'jotus'), "<span class=\"edit-link\">", "</span>"); ?>
+                        <?php edit_post_link(__('Edit', 'jotus'), "<span class=\"edit-link\">", "</span>"); ?>
                     </div>
 
                 </article>
